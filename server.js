e56a587710dd0efa5031c8289749ed61b8982014
Samuel Theophilus <samueltheophilus019@gmail.com>
2023-06-28 12:20:29 +0100

    server file

---
+++ server.js
@@ -0,0 +1,12 @@
+const express = require('express');
+
+const app = express();
+const port = process.env.PORT || 5000;
+const routes = require('./routes');
+
+app.use(express.json());
+app.use('/', routes);
+
+app.listen(port, () => {
+  console.log(`Server is running on port ${port}`);
+});
