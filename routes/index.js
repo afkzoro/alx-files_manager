8191d97afbf669df541003be5b60d01e0e9e16a6
Samuel Theophilus <samueltheophilus019@gmail.com>
2023-06-28 12:21:53 +0100

    routes file

---
+++ routes/index.js
@@ -0,0 +1,22 @@
+const express = require('express');
+const UsersController = require('../controllers/UsersController');
+const AuthController = require('../controllers/AuthController');
+const FilesController = require('../controllers/FilesController');
+
+const router = express.Router();
+
+// User routes
+router.post('/users', UsersController.postNew);
+
+// Authentication routes
+router.get('/connect', AuthController.getConnect);
+router.get('/disconnect', AuthController.getDisconnect);
+router.get('/users/me', AuthController.getMe);
+
+// File routes
+router.post('/files', FilesController.postUpload);
+router.get('/files/:id', FilesController.getShow);
+router.get('/files', FilesController.getIndex);
+router.put('/files/:id/publish', FilesController.putPublish);
+router.put('/files/:id/unpublish', FilesController.putUnpublish);
+module.exports = router;
