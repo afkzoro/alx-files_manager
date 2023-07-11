5e65e88f463ca63b5f6e700d74c4e141ae1f1fc8
Samuel Theophilus <samueltheophilus019@gmail.com>
2023-07-11 07:22:00 +0100

    test

--- utils/db.js
+++ utils/db.js
@@ -1,75 +1,40 @@
-/* eslint-disable */
-const mongoose = require('mongoose');
-const dotenv = require('dotenv');
+import { MongoClient } from 'mongodb';
 
-dotenv.config();
+const host = process.env.DB_HOST || 'localhost';
+const port = process.env.DB_PORT || 27017;
+const database = process.env.DB_DATABASE || 'files_manager';
+const url = `mongodb://${host}:${port}/`;
 
 class DBClient {
   constructor() {
-    const { DB_HOST = '0.0.0.0', DB_PORT = 27017, DB_DATABASE = 'files_manager' } = process.env;
-    const url = `mongodb://${DB_HOST}:${DB_PORT}/${DB_DATABASE}`;
-
-    this.connect(url);
-
-    const userSchema = new mongoose.Schema({
-      email: {
-        type: String,
-        required: true,
-        unique: true,
-      },
-      password: {
-        type: String,
-        required: true,
-      },
+    this.db = null;
+    MongoClient.connect(url, { useUnifiedTopology: true }, (error, client) => {
+      if (error) console.log(error);
+      this.db = client.db(database);
+      this.db.createCollection('users');
+      this.db.createCollection('files');
     });
-
-    const filesSchema = new mongoose.Schema({
-      userId: { type: String, required: true },
-      name: { type: String, required: true },
-      type: { type: String, required: true },
-      isPublic: { type: Boolean, default: false },
-      parentId: { type: String, default: '0' },
-      localPath: { type: String },
-    });
-
-    this.users = mongoose.model('User', userSchema);
-    this.files = mongoose.model('File', filesSchema);
-  }
-
-  async connect(url) {
-    try {
-      await mongoose.connect(url, { useNewUrlParser: true, useUnifiedTopology: true });
-      console.log('Connected to MongoDB');
-    } catch (error) {
-      console.error('Error connecting to MongoDB:', error);
-      process.exit(1);
-    }
   }
 
   isAlive() {
-    return mongoose.connection.readyState === 1;
+    return !!this.db;
   }
 
   async nbUsers() {
-    try {
-      const count = await this.users.countDocuments();
-      return count;
-    } catch (error) {
-      console.error('Error retrieving the number of users:', error);
-      throw error;
-    }
+    return this.db.collection('users').countDocuments();
+  }
+
+  async getUser(query) {
+    console.log('QUERY IN DB.JS', query);
+    const user = await this.db.collection('users').findOne(query);
+    console.log('GET USER IN DB.JS', user);
+    return user;
   }
 
   async nbFiles() {
-    try {
-      const count = await this.files.countDocuments();
-      return count;
-    } catch (error) {
-      console.error('Error retrieving the number of files:', error);
-      throw error;
-    }
+    return this.db.collection('files').countDocuments();
   }
 }
 
 const dbClient = new DBClient();
-module.exports = dbClient;
+export default dbClient;
